b7e821a77e83ae199da94914a742a7f167850976
Ariel Martinez <ariel.mtz85@gmail.com>
2025-05-18 00:07:02 -0300

    creando estructura basica, agregando librerias, creando componente noticias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
