0c245ec2b4f319a7b2b6db1c820baf90efda684c
Ariel Martinez <ariel.mtz85@gmail.com>
2025-05-20 17:54:18 -0300

    agregando componente y servicio carspecs

--- src/app/pages/car-specs/car-specs.component.ts
+++ src/app/pages/car-specs/car-specs.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import {  CarSpecsService } from '../../services/car-specs/car-specs.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-car-specs',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './car-specs.component.html',
   styleUrl: './car-specs.component.css'
 })
@@ -14,6 +16,9 @@ export class CarSpecsComponent implements OnInit {
 
   // Array para almacenar la respuesta de la API
   carSpecsResponse: any[] = [];
+
+  // Variable para almacenar el id del auto seleccionado
+  selectedCar: number = 0;
 
   ngOnInit(): void { 
     this.getCarSpecs();
@@ -31,6 +36,34 @@ export class CarSpecsComponent implements OnInit {
     });
     console.log('Obteniendo especificaciones de autos...');
   }
+
+  // Array para almacenar los modelos de autos
+  carSpecsModelResponse: any[] = [];
+
+  // Variable para almacenar el id del auto seleccionado
+  selectedCarId: number = 0;
+
+  // Metodo para obtener los modelos de autos
+  getCarSpecsModel(id: number) { 
+    this.carSpecs.getCarSpecsModel(id).subscribe({
+      next: (response) => { 
+        this.carSpecsModelResponse = response;
+        console.log('Modelos de autos:', this.carSpecsModelResponse);
+      },
+      error: (error) => { 
+        console.error('Error al obtener modelos de autos:', error);
+      }
+    });
+    console.log('Obteniendo modelos de autos...');
+  }
+
+  // funcion para obtener el id del auto seleccionado
+  getSelectedCarId(id: number) { 
+    this.carSpecsModelResponse = [];
+    this.selectedCarId = 0;
+    this.getCarSpecsModel(id);
+  }
+
 }
 
 
--- src/app/services/car-specs/car-specs.service.ts
+++ src/app/services/car-specs/car-specs.service.ts
@@ -1,11 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 
 
-// interface que representa la respueste de la api
+// interface que representa la respueste de la api, los id y marcas de los autos
 export interface CarSpecsResponse { 
+  id: number;
+  name: string;
+}
+
+// interface que representa la respuesta de la api, los modelos de los autos
+
+export interface CarSpecsModelResponse { 
   id: number;
   name: string;
 }
@@ -16,6 +24,10 @@ export interface CarSpecsResponse {
 })
 export class CarSpecsService {
 
+  // creo una variable para guardar la respuesta de la api en "cache"
+
+  private marcasEnCache: CarSpecsResponse[] | null = null;
+
   constructor(private http: HttpClient) { }
 
   // url de la api
@@ -24,6 +36,14 @@ export class CarSpecsService {
   // metodo para obtener la respuesta de la api
 
   getCarSpecs(): Observable<CarSpecsResponse[]> { 
+
+    //si la respuesta ya esta en cache, la devolvemos
+    if (this.marcasEnCache) { 
+      console.log('Devolviendo datos de cache');
+      console.log(this.marcasEnCache);
+      return of(this.marcasEnCache);
+    }
+
     //headers de la api
     const httpOptions = {
       headers: {
@@ -32,7 +52,35 @@ export class CarSpecsService {
       }
     };
     //llamamos a la api y devolvemos la respuesta
-    return this.http.get<CarSpecsResponse[]>(this.apiUrl, httpOptions);
+    return this.http.get<CarSpecsResponse[]>(this.apiUrl, httpOptions)
+      .pipe(tap((data) => this.marcasEnCache = data), // guardamos la respuesta en cache
+    
+    //si hay un error, devolvemos un array vacio para evitar errores en la aplicacion
+        catchError((error) => { 
+          console.error('Error al obtener datos de la API:', error);
+          return of([]);
+        })
+  
+      );
+  }
+  
+
+  // metodo para obtener los modelos de los autos
+  getCarSpecsModel(id: number): Observable<CarSpecsModelResponse[]> { 
+    const apiUrl = `https://car-specs.p.rapidapi.com/v2/cars/makes/${id}/models`;
+    const httpOptions = {
+      headers: {
+        'x-rapidapi-key': '0d67c15fc8mshf05bd9e4f6655bfp1a0bd6jsn30b062fd0d5c',
+        'x-rapidapi-host': 'car-specs.p.rapidapi.com'
+      }
+    };
+    return this.http.get<CarSpecsModelResponse[]>(apiUrl, httpOptions)
+      .pipe(
+        catchError((error) => { 
+          console.error('Error al obtener datos de la API:', error);
+          return of([]);
+        })
+      );
   }
 
 
