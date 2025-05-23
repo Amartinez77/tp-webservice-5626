a78e9e9b5cd54145f1619070d7519d78c3028369
Ariel Martinez <ariel.mtz85@gmail.com>
2025-05-23 14:13:54 -0300

    corrigiendo error al mostrar los datos en el modal

--- src/app/pages/car-specs/car-specs.component.ts
+++ src/app/pages/car-specs/car-specs.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
-import {  CarSpecsService } from '../../services/car-specs/car-specs.service';
+import {  CarSpecsbyidResponse, CarSpecsModelIdResponse, CarSpecsModelResponse, CarSpecsResponse, CarSpecsService, CarSpecsTrimIdResponse } from '../../services/car-specs/car-specs.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+declare var bootstrap: any; // Declarar la variable bootstrap para evitar errores de compilación
 @Component({
   selector: 'app-car-specs',
   imports: [FormsModule, CommonModule],
@@ -14,34 +15,69 @@ export class CarSpecsComponent implements OnInit {
 
   constructor(private carSpecs: CarSpecsService) { }
 
-  // Array para almacenar la respuesta de la API
-  carSpecsResponse: any[] = [];
-
-  // Variable para almacenar el id del auto seleccionado
-  selectedCar: number = 0;
+  
 
   ngOnInit(): void { 
     this.getCarSpecs();
   }
 
-  getCarSpecs() { 
-    this.carSpecs.getCarSpecs().subscribe({
-      next: (response) => { 
-        this.carSpecsResponse = response;
-        console.log('Especificaciones de autos:', this.carSpecsResponse);
-      },
-      error: (error) => { 
-        console.error('Error al obtener especificaciones de autos:', error);
-      }
-    });
-    console.log('Obteniendo especificaciones de autos...');
-  }
+  // Array para almacenar la respuesta de la API
+  carSpecsResponse: CarSpecsResponse[] = [];
 
   // Array para almacenar los modelos de autos
-  carSpecsModelResponse: any[] = [];
+  carSpecsModelResponse: CarSpecsModelResponse[] = [];
 
-  // Variable para almacenar el id del auto seleccionado
+  // Array para almacenar la serie de autos
+  carSpecsModelIdResponse: CarSpecsModelIdResponse[] = [];
+
+  // Array para almacenar el id trim de la serie de autos
+  carSpecsTrimIdResponse: CarSpecsTrimIdResponse[] = [];
+
+  // Array para almacenar las especificaciones de autos
+  carSpecsbyidResponse: CarSpecsbyidResponse | null = null;
+
+  // Variable para almacenar el id de la marca seleccionada
+  selectedCar: number = 0;
+  
+  // Variable para almacenar el id del modelo de auto seleccionado
   selectedCarId: number = 0;
+
+  // Variable para almacenar el id de la serie seleccionada
+  selectedCarTrim: number = 0;
+  selectedCarGenerationId: number = 0;
+
+  // Variable para almacenar el id trim de la serie seleccionada
+  selectedCarTrimId: number = 0;
+  
+  // Variable para almacenar el id del modelo especifico y obtener las especificaciones
+  selectedCarModel: number = 0;
+
+    // metodo para obtener las marcas de autos
+    getCarSpecs() { 
+      this.carSpecs.getCarSpecs().subscribe({
+        next: (response) => { 
+          this.carSpecsResponse = response;
+          console.log('Especificaciones de autos:', this.carSpecsResponse);
+        },
+        error: (error) => { 
+          console.error('Error al obtener especificaciones de autos:', error);
+        }
+      });
+      console.log('Obteniendo especificaciones de autos...');
+  }
+  
+  // funcion para obtener el id de la marca del auto seleccionado desde el html
+  getSelectedCarId(id: number) { 
+    this.selectedCar = id;
+    this.selectedCarId = 0;
+    this.selectedCarTrim = 0;
+    this.selectedCarGenerationId = 0;
+    this.carSpecsModelResponse = [];
+    this.carSpecsModelIdResponse = [];
+    this.carSpecsTrimIdResponse = [];
+    this.getCarSpecsModel(id);
+    console.log('Marca seleccionada:', id);
+  }
 
   // Metodo para obtener los modelos de autos
   getCarSpecsModel(id: number) { 
@@ -56,13 +92,94 @@ export class CarSpecsComponent implements OnInit {
     });
     console.log('Obteniendo modelos de autos...');
   }
-
-  // funcion para obtener el id del auto seleccionado
-  getSelectedCarId(id: number) { 
-    this.carSpecsModelResponse = [];
-    this.selectedCarId = 0;
-    this.getCarSpecsModel(id);
+  // funcion para obtener el id del modelo de auto seleccionado desde el html
+  getSelectedCarModelId(id: number) { 
+    this.carSpecsModelIdResponse = [];
+    this.selectedCarModel = 0;
+    this.getCarSpecsModelId(id);
+    console.log('modelo seleccionado:', id);
   }
+  
+
+  // metodo para obtener el id de la generacion del auto 
+  getCarSpecsModelId(id: number) { 
+    this.selectedCarId = id;
+    this.selectedCarTrim = 0;
+    this.selectedCarGenerationId = 0;
+    this.carSpecsModelIdResponse = [];
+    this.carSpecsTrimIdResponse = [];
+
+
+    this.carSpecs.getCarSpecsModelId(id).subscribe({
+      next: (response) => { 
+        this.carSpecsModelIdResponse = response;
+        console.log('Serie de autos:', response);
+      },
+      error: (error) => { 
+        console.error('Error al obtener serie de autos:', error);
+      }
+    });
+    console.log('Obteniendo serie de autos...');
+  }
+
+    // funcion para obtener el id trim de la generacion seleccionada desde el html
+    getSelectedCarTrimId(id: number) { 
+      this.carSpecsTrimIdResponse = [];
+      this.selectedCarTrimId = 0;
+      this.getCarSpecsTrimId(id);
+      console.log('serie seleccionada:', id);
+    }
+  
+
+  // metodo para obtener el id trim de la serie seleccionada
+  getCarSpecsTrimId(id: number) { 
+
+    this.selectedCarTrim = id;
+    this.selectedCarGenerationId = 0;
+    this.carSpecsTrimIdResponse = [];
+
+    this.carSpecs.getCarSpecsTrimId(id).subscribe({
+      next: (response) => { 
+        this.carSpecsTrimIdResponse = response;
+        console.log('Id trim de la serie:', response);
+      },
+      error: (error) => { 
+        console.error('Error al obtener id trim de la serie:', error);
+      }
+    });
+    console.log('Obteniendo id trim de la serie...');
+  }
+
+  // funcion para obtener el id trim del auto especifico seleccionado desde el html
+  getSelectedCarTrim(id: number) { 
+    //this.carSpecsbyidResponse = null;
+    //this.selectedCarTrim = 0;
+    //this.getCarSpecsById(id);
+    this.selectedCarGenerationId = id;
+    console.log('generacion seleccionada:', id);
+  }
+
+
+  // metodo para obtener las especificaciones del auto segun el id
+  getCarSpecsById(id: number) { 
+    this.carSpecs.getCarSpecsbyid(id).subscribe({
+      next: (response) => { 
+        this.carSpecsbyidResponse = response || null;
+        console.log('Especificaciones de autos en el metodo getCarSpecsById:', response);
+        console.log('Especificaciones de autos:', response);
+
+        //abrir el modal manualmente despues de obtener la respuesta
+        const modal = new bootstrap.Modal(document.getElementById('especificacionesModal')!);
+        modal.show();
+
+      },
+      error: (error) => { 
+        console.error('Error al obtener especificaciones de autos:', error);
+      }
+    });
+    
+  }
+
 
 }
 
--- src/app/services/car-specs/car-specs.service.ts
+++ src/app/services/car-specs/car-specs.service.ts
@@ -10,23 +10,76 @@ export interface CarSpecsResponse {
   id: number;
   name: string;
 }
-
 // interface que representa la respuesta de la api, los modelos de los autos
-
 export interface CarSpecsModelResponse { 
   id: number;
   name: string;
 }
 
+// interface que representa la respuesta de la api, las generaciones de los autos
+export interface CarSpecsModelIdResponse { 
+  id: number;
+  name: string;
+  generation: string;
+  trim: string;
+}
+
+// interface que representa la respuesta de la api, los id trim de los autos
+export interface CarSpecsTrimIdResponse { 
+  id: number;
+  series: string;
+}
+
+// interface que representa la respuesta de la api, las especificaciones de los autos
+export interface CarSpecsbyidResponse { 
+  id: number;
+  make: string;
+  model: string;
+  generation: string;
+  series: string;
+  trim: string;
+  bodyType: string;
+  loadHeightMm: number;
+  numberOfSeats: number;
+  lengthMm: number;
+  widthMm: number;
+  heightMm: number;
+  wheelSizeR14: number;
+  fullWeightKg: number;
+  maximumTorqueNM: number;
+  injectionType: string;
+  cylinderLayout: string;
+  numberOfCylinders: number;
+  engineType: string;
+  boostType: string;
+  enginePlacement: string;
+  maxPowerKw: number;
+  engineHp: number;
+  transmission: string;
+  cityFuelPer100KmL: number;
+  mixedFuelConsumptionPer100KmL: number;
+  highwayFuelPer100KmL: number;
+  fuelTankCapacityL: number;
+  acceleration0To100KmPerHS: number;
+  maxSpeedKmPerH: number;
+  countryOfOrigin: string;
+  numberOfDoors: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarSpecsService {
 
-  // creo una variable para guardar la respuesta de la api en "cache"
+  // variable para la key de la api
+  private keyApi = 'ab59360f99msh8f50eff1a46dd09p1b3b38jsndd6592c55dcd';
 
+  // creo una variable para guardar la respuesta de la api en "cache"
   private marcasEnCache: CarSpecsResponse[] | null = null;
+  private marcasModelEnCache: CarSpecsModelResponse[] | null = null;
+  private marcasModelIdEnCache: CarSpecsModelIdResponse[] | null = null;
+  private marcasTrimIdEnCache: CarSpecsTrimIdResponse[] | null = null;
+  private especificacionesEnCache: CarSpecsbyidResponse | null = null;
 
   constructor(private http: HttpClient) { }
 
@@ -47,7 +100,7 @@ export class CarSpecsService {
     //headers de la api
     const httpOptions = {
       headers: {
-        'x-rapidapi-key': '0d67c15fc8mshf05bd9e4f6655bfp1a0bd6jsn30b062fd0d5c',
+        'x-rapidapi-key': this.keyApi,
         'x-rapidapi-host': 'car-specs.p.rapidapi.com'
       }
     };
@@ -67,14 +120,50 @@ export class CarSpecsService {
 
   // metodo para obtener los modelos de los autos
   getCarSpecsModel(id: number): Observable<CarSpecsModelResponse[]> { 
+
+    //si la respuesta ya esta en cache, la devolvemos
+    if (this.marcasModelEnCache) { 
+      console.log('Devolviendo datos de cache');
+      console.log(this.marcasModelEnCache);
+      return of(this.marcasModelEnCache);
+    }
+
+
     const apiUrl = `https://car-specs.p.rapidapi.com/v2/cars/makes/${id}/models`;
     const httpOptions = {
       headers: {
-        'x-rapidapi-key': '0d67c15fc8mshf05bd9e4f6655bfp1a0bd6jsn30b062fd0d5c',
+        'x-rapidapi-key':this.keyApi,
         'x-rapidapi-host': 'car-specs.p.rapidapi.com'
       }
     };
     return this.http.get<CarSpecsModelResponse[]>(apiUrl, httpOptions)
+      .pipe(tap((data) => this.marcasModelEnCache = data), // guardamos la respuesta en cache
+        
+        //si hay un error, devolvemos un array vacio para evitar errores en la aplicacion
+        catchError((error) => { 
+          console.error('Error al obtener datos de la API:', error);
+          return of([]);
+        })
+      );
+  }
+
+  // metodo para obtener la serie segun el id de modelo del auto
+  getCarSpecsModelId(id: number): Observable<CarSpecsModelIdResponse[]> { 
+    //si la respuesta ya esta en cache, la devolvemos
+    if (this.marcasModelIdEnCache) { 
+      console.log('Devolviendo datos de cache');
+      console.log(this.marcasModelIdEnCache);
+      return of(this.marcasModelIdEnCache);
+    }
+
+    const apiUrl = `https://car-specs.p.rapidapi.com/v2/cars/models/${id}/generations`;
+    const httpOptions = {
+      headers: {
+        'x-rapidapi-key': this.keyApi,
+        'x-rapidapi-host': 'car-specs.p.rapidapi.com'
+      }
+    };
+    return this.http.get<CarSpecsModelIdResponse[]>(apiUrl, httpOptions)
       .pipe(
         catchError((error) => { 
           console.error('Error al obtener datos de la API:', error);
@@ -83,5 +172,59 @@ export class CarSpecsService {
       );
   }
 
+  // metodo para obtener en base al id de la serie el id trim
+  getCarSpecsTrimId(id: number): Observable<CarSpecsTrimIdResponse[]> { 
+    //si la respuesta ya esta en cache, la devolvemos
+    if (this.marcasTrimIdEnCache) { 
+      console.log('Devolviendo datos de cache');
+      console.log(this.marcasTrimIdEnCache);
+      return of(this.marcasTrimIdEnCache);
+    }
+
+    const apiUrl = `https://car-specs.p.rapidapi.com/v2/cars/generations/${id}/trims`;
+    const httpOptions = {
+      headers: {
+        'x-rapidapi-key': this.keyApi,
+        'x-rapidapi-host': 'car-specs.p.rapidapi.com'
+      }
+    };
+    return this.http.get<CarSpecsTrimIdResponse[]>(apiUrl, httpOptions)
+      .pipe(
+        tap((data) => this.marcasTrimIdEnCache = data), // guardamos la respuesta en cache
+        catchError((error) => { 
+          console.error('Error al obtener datos de la API:', error);
+          return of([]);
+        })
+      );
+  }
+
+
+
+
+  // metodo para obtener las especificaciones del auto segun el id trim
+  getCarSpecsbyid(id: number): Observable<CarSpecsbyidResponse | null> { 
+    //si la respuesta ya esta en cache, la devolvemos
+    if (this.especificacionesEnCache) { 
+      console.log('Devolviendo datos de cache');
+      console.log(this.especificacionesEnCache);
+      return of(this.especificacionesEnCache);
+    }
+
+    const apiUrl = `https://car-specs.p.rapidapi.com/v2/cars/trims/${id}`;
+    const httpOptions = {
+      headers: {
+        'x-rapidapi-key': this.keyApi,
+        'x-rapidapi-host': 'car-specs.p.rapidapi.com'
+      }
+    };
+    return this.http.get<CarSpecsbyidResponse>(apiUrl, httpOptions)
+      .pipe(
+        tap((data) => this.especificacionesEnCache = data), // guardamos la respuesta en cache
+        catchError((error) => { 
+          console.error('Error al obtener datos de la API:', error);
+          return of(null);
+        })
+      );
+  }
 
 }
